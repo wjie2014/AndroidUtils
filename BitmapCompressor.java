d1b86d33744f8a7aef8cfeb0244493a1073d033e
wjie2014 <489657152@qq.com>
2016-05-25 15:47:08 +0800

    图片无损压缩工具
    图片无损压缩

---
+++ BitmapCompressor.java
@@ -0,0 +1,154 @@
+package cn.stuyou.Utils;
+
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * 基本功能：图片压缩工具
+ * 创建：王杰
+ * 创建时间：2015-05-13
+ */
+public class BitmapCompressor {
+
+    /**
+     * Bitmap对象保存为图片文件，压缩
+     */
+
+    public static File saveBitmapFile(Bitmap bitmap, String filePath) {
+        File file = new File(filePath);//将要保存图片的路径
+        try {
+            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 80, bos);
+            bos.flush();
+            bos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return file;
+    }
+
+    /**
+     * Bitmap对象保存为图片文件，不压缩
+     */
+
+    public static File saveBitmapFileOnly(Bitmap bitmap, String filePath) {
+        File file = new File(filePath);//将要保存图片的路径
+        try {
+            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+            bos.flush();
+            bos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return file;
+    }
+
+
+    /**
+     * 计算图片的缩放值
+     *
+     * @param options
+     * @param reqWidth
+     * @param reqHeight
+     * @return
+     */
+    public static int calculateInSampleSize(BitmapFactory.Options options,
+                                            int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        int height = options.outHeight;
+        int width = options.outWidth;
+        int inSampleSize = 1;
+        int tempHeight;
+        int tempWidth;
+        // 判断高度和宽度，将尺寸大的转换为高度，尺寸小的转换为宽度
+        if (height < width) {
+            tempHeight = width;
+            tempWidth = height;
+            height = tempHeight;
+            width = tempWidth;
+        }
+
+        if (height > reqHeight || width > reqWidth) {
+
+            // Calculate ratios of height and width to requested height and
+            // width
+            final int heightRatio = Math.round((float) height
+                    / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+
+            // Choose the smallest ratio as inSampleSize value, this will
+            // guarantee
+            // a final image with both dimensions larger than or equal to the
+            // requested height and width.
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+
+        return inSampleSize;
+    }
+
+
+    /**
+     * 根据路径获得突破并压缩返回bitmap用于显示
+     *
+     * @return
+     */
+    public static Bitmap getSmallBitmap(String filePath) {
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(filePath, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, 480, 800);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+
+        return BitmapFactory.decodeFile(filePath, options);
+    }
+
+    /**
+     * 根据路径删除图片
+     *
+     * @param path
+     */
+    public static void deleteTempFile(String path) {
+        File file = new File(path);
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+
+    /**
+     * 获取保存图片的目录
+     *
+     * @return
+     */
+    public static File getAlbumDir() {
+        File dir = new File(
+                Environment
+                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                getAlbumName());
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        return dir;
+    }
+
+    /**
+     * 获取保存 隐患检查的图片文件夹名称
+     *
+     * @return
+     */
+    public static String getAlbumName() {
+        return "knkw";
+    }
+}
+
